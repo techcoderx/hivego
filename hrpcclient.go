60334c879f7d100a38d670444d33e1458ae41363
sagar kothari <sag333ar@gmail.com>
2025-12-10 06:13:11 +0530

    calling api from alternate node

--- hrpcclient.go
+++ hrpcclient.go
@@ -2,16 +2,25 @@ package hivego
 
 import (
 	"errors"
-	"strconv"
+	"sync"
 
 	"github.com/cfoxon/jsonrpc2client"
 )
 
+type NodeStats struct {
+	successCount int
+	failureCount int
+	rollingAvg   float64
+}
+
 type HiveRpcNode struct {
-	address     string
-	MaxConn     int
-	MaxBatch    int
-	NoBroadcast bool
+	addresses      []string
+	currentIndex   int
+	nodeStats      []NodeStats
+	mutex          sync.RWMutex
+	MaxConn        int
+	MaxBatch       int
+	NoBroadcast    bool
 }
 
 type globalProps struct {
@@ -25,57 +34,125 @@ type hrpcQuery struct {
 	params interface{}
 }
 
-func NewHiveRpc(addr string) *HiveRpcNode {
-	return NewHiveRpcWithOpts(addr, 1, 1)
+func NewHiveRpc(addrs []string) *HiveRpcNode {
+	return NewHiveRpcWithOpts(addrs, 1, 1)
 }
 
-func NewHiveRpcWithOpts(addr string, maxConn int, maxBatch int) *HiveRpcNode {
-	return &HiveRpcNode{address: addr,
-		MaxConn:  maxConn,
-		MaxBatch: maxBatch,
+func NewHiveRpcWithOpts(addrs []string, maxConn int, maxBatch int) *HiveRpcNode {
+	nodeStats := make([]NodeStats, len(addrs))
+	return &HiveRpcNode{
+		addresses:    addrs,
+		currentIndex: 0,
+		nodeStats:    nodeStats,
+		MaxConn:      maxConn,
+		MaxBatch:     maxBatch,
 	}
 }
 
 func (h *HiveRpcNode) GetDynamicGlobalProps() ([]byte, error) {
 	q := hrpcQuery{method: "condenser_api.get_dynamic_global_properties", params: []string{}}
-	res, err := h.rpcExec(h.address, q)
+	res, err := h.rpcExec(q)
 	if err != nil {
 		return nil, err
 	}
 	return res, nil
 }
 
-func (h *HiveRpcNode) rpcExec(endpoint string, query hrpcQuery) ([]byte, error) {
-	rpcClient := jsonrpc2client.NewClientWithOpts(endpoint, h.MaxConn, h.MaxBatch)
-	jr2query := &jsonrpc2client.RpcRequest{Method: query.method, JsonRpc: "2.0", Id: 1, Params: query.params}
-	resp, err := rpcClient.CallRaw(jr2query)
-	if err != nil {
-		return nil, err
+func (h *HiveRpcNode) rpcExec(query hrpcQuery) ([]byte, error) {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+
+	numNodes := len(h.addresses)
+	for i := 0; i < numNodes; i++ {
+		index := (h.currentIndex + i) % numNodes
+		endpoint := h.addresses[index]
+
+		rpcClient := jsonrpc2client.NewClientWithOpts(endpoint, h.MaxConn, h.MaxBatch)
+		jr2query := &jsonrpc2client.RpcRequest{Method: query.method, JsonRpc: "2.0", Id: 1, Params: query.params}
+		resp, err := rpcClient.CallRaw(jr2query)
+		if err != nil {
+			h.nodeStats[index].failureCount++
+			h.updateRollingAvg(index)
+			continue
+		}
+
+		if resp.Error != nil {
+			h.nodeStats[index].failureCount++
+			h.updateRollingAvg(index)
+			continue
+		}
+
+		// Check for bad data: if result is empty, consider it bad
+		if len(resp.Result) == 0 {
+			h.nodeStats[index].failureCount++
+			h.updateRollingAvg(index)
+			continue
+		}
+
+		// Success
+		h.nodeStats[index].successCount++
+		h.updateRollingAvg(index)
+		h.currentIndex = index // Set to last successful node
+		return resp.Result, nil
 	}
 
-	if resp.Error != nil {
-		return nil, errors.New(strconv.Itoa(resp.Error.Code) + "    " + resp.Error.Message)
-	}
-
-	return resp.Result, nil
+	return nil, errors.New("all API nodes failed")
 }
 
-func (h *HiveRpcNode) rpcExecBatchFast(endpoint string, queries []hrpcQuery) ([][]byte, error) {
-	rpcClient := jsonrpc2client.NewClientWithOpts(endpoint, h.MaxConn, h.MaxBatch)
+func (h *HiveRpcNode) updateRollingAvg(index int) {
+	total := h.nodeStats[index].successCount + h.nodeStats[index].failureCount
+	if total > 0 {
+		h.nodeStats[index].rollingAvg = float64(h.nodeStats[index].successCount) / float64(total)
+	}
+}
 
-	var jr2queries jsonrpc2client.RPCRequests
-	for i, query := range queries {
-		jr2query := &jsonrpc2client.RpcRequest{Method: query.method, JsonRpc: "2.0", Id: i, Params: query.params}
-		jr2queries = append(jr2queries, jr2query)
+func (h *HiveRpcNode) rpcExecBatchFast(queries []hrpcQuery) ([][]byte, error) {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+
+	numNodes := len(h.addresses)
+	for i := 0; i < numNodes; i++ {
+		index := (h.currentIndex + i) % numNodes
+		endpoint := h.addresses[index]
+
+		rpcClient := jsonrpc2client.NewClientWithOpts(endpoint, h.MaxConn, h.MaxBatch)
+
+		var jr2queries jsonrpc2client.RPCRequests
+		for j, query := range queries {
+			jr2query := &jsonrpc2client.RpcRequest{Method: query.method, JsonRpc: "2.0", Id: j, Params: query.params}
+			jr2queries = append(jr2queries, jr2query)
+		}
+
+		resps, err := rpcClient.CallBatchFast(jr2queries)
+		if err != nil {
+			h.nodeStats[index].failureCount++
+			h.updateRollingAvg(index)
+			continue
+		}
+
+		// Check if any response has error or bad data
+		hasError := false
+		for _, respBytes := range resps {
+			if len(respBytes) == 0 {
+				hasError = true
+				break
+			}
+		}
+		if hasError {
+			h.nodeStats[index].failureCount++
+			h.updateRollingAvg(index)
+			continue
+		}
+
+		// Success
+		h.nodeStats[index].successCount++
+		h.updateRollingAvg(index)
+		h.currentIndex = index
+
+		var batchResult [][]byte
+		batchResult = append(batchResult, resps...)
+		return batchResult, nil
 	}
 
-	resps, err := rpcClient.CallBatchFast(jr2queries)
-	if err != nil {
-		return nil, err
-	}
-
-	var batchResult [][]byte
-	batchResult = append(batchResult, resps...)
-
-	return batchResult, nil
+	return nil, errors.New("all API nodes failed")
 }
